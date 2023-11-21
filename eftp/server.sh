deeff8690e0ccc6bf2809f4ac562e63de43c1d89
ArnauuEnti <arnau.juve@enti.cat>
2023-11-21 10:21:27 +0100

    primeros pasos en el cliente/servidor del protocolo EFTP

---
+++ eftp/server.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+CLIENT="localhost"
+
+echo "servidor de EFTP"
+
+echo "(0) Listen"
+
+DATA=`nc -l -p 3333 -w 0`
+
+echo $DATA
+
+echo "(3) Test & Send"
+
+if [ "$DATA" != "EFTP 1.0" ]
+then
+
+	echo "ERROR 1: BAD HEADER"
+	sleep 1
+	echo "KO_HEADER" | nc $CLIENT 3333
+	exit 1
+fi
+
+
+echo "OK_HEADER"
+sleep 1
+echo "OK_HEADER" | nc $CLIENT 3333
+
+echo "(4) Listen"
+DATA=`nc -l -p 3333 -w 0`
+
+echo $DATA
+
+echo "(7) Test & Send"
+
+if [ "$DATA != "BOOM" ]
+then
+	echo "ERROR 2: BAD HANDSHAKE"
+	Sleep 1
+	echo"KO_HANDSHAKE" | nc $CLIENT 3333
+	exit 2
+fi
+
+echo "OK_HANDSHAKE"
+Sleep 1
+echo "OK_HANDSHAKE" | nc $CLIENT 3333
+
+echo "(8) Listen"
+DATA=`nc -l -p 3333 -w 0`
